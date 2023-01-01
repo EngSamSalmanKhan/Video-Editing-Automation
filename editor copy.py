6af605465c2fa4920035fd76a1a3edef476300e2
symilar <symilarshelp@gmail.com>
2023-01-01 03:36:25 -0700

    Menu, video moving and option for split sec Added

--- editor copy.py
+++ editor copy.py
@@ -17,54 +17,96 @@ log.basicConfig(
 
 
 def videoDivider(path):
+    eachVideoDuration = 180
+    FullSPlittedFolder = "F:\\Projects\\Python\\bots\\VideoEditor\\Video Editor\\videos\\FullSplitted\\"
+
+    SplittedFolder = "F:\\Projects\\Python\\bots\\VideoEditor\\Video Editor\\videos\\splited\\"
+
+    try:
+        # check if path exists
+        if os.path.exists(path):
+            print("Path exists")
+            if os.path.isfile(path):
+                print("Path is a file")
+    except Exception as e:
+        log.error(e)
+        print("Error occured while checking the path")
+
     # name of the video without the extension
     title = os.path.splitext(os.path.basename(path))[0]
-    # get the first three words of the video name
+    # # get the first three words of the video name
     title = title.split(" ", 3)[0:3]
-    # join the words with a space
-    title = " ".join(title) + "..."
+    # # join the words
+    title = " ".join(title)
 
     videoduration = VideoFileClip(path).duration
-    if videoduration <= 180:
-        pass
+    if videoduration <= eachVideoDuration:
+        print("Video Duration is less than " +
+              str(eachVideoDuration) + " seconds")
     else:
         # create multiple videos
         # get the number of videos to be created
-        numberofvideos = int(videoduration/180)
+        numberofvideos = int(videoduration/eachVideoDuration)
         # get the remaining time
-        remainingtime = videoduration % 180
+        remainingtime = videoduration % eachVideoDuration
         # create the videos
         for i in range(numberofvideos):
             # create the video
 
             # get the start time
-            start = i*180
+            start = i*eachVideoDuration
             # get the end time
-            end = (i+1)*180
+            end = (i+1)*eachVideoDuration
             # create the video
-            clip = VideoFileClip(path).subclip(start, end)
-            clip.write_videofile("./videos/splited/" + title + "Part " +
-                                 str(i+1)+".mp4", fp=24)
+            print("Creating " + str(i+1) + " video from " +
+                  str(start) + " to " + str(end))
 
-        clip = VideoFileClip(path).subclip(numberofvideos*180, videoduration)
-        clip.write_videofile("./videos/splited/" + title + "Part " +
+            clip = VideoFileClip(path).subclip(start, end)
+            try:
+                print("Writing video file ")
+                clip.write_videofile(SplittedFolder + title + " Part " +
+                                     str(i+1)+".mp4", fps=24)
+            except Exception as e:
+                log.error(e)
+                print(e)
+                print("Error occured while Writing the video")
+
+        clip = VideoFileClip(path).subclip(
+            numberofvideos*eachVideoDuration, videoduration)
+        clip.write_videofile(SplittedFolder + title + " Part " +
                              str(numberofvideos+1)+".mp4", fps=24)
+        # move video to the splited folder
+        try:
+            moveVideo(path, FullSPlittedFolder)
+        except Exception as e:
+            log.error(e)
+            print(e)
+            print("Error occured while moving the video")
+        print("Video Splitted\n\n")
 
 
 def editor(paths):
+    FullEditedFolder = "F:\\Projects\\Python\\bots\\VideoEditor\\Video Editor\\videos\FullEdited\\"
+    ChannelName = "Village Tale Food".upper()
+    print("Channel: " + ChannelName)
     try:
         # clip for the video
         clip = VideoFileClip(paths)
         # flip the video
-        clip = clip.fx(vfx.mirror_x)
+        # clip = clip.fx(vfx.mirror_x) #flip mirror video
         clip = clip.fx(vfx.colorx, 1.2)
         clip = clip.fx(afx.audio_normalize)
 
- #
 #
 #
 #
+#
+
         title = os.path.splitext(os.path.basename(paths))[0]
+        # get 5 words of the video name
+        title = title.split(" ", 5)[0:5]
+        title = " ".join(title).upper()
+
         print(title)
         width = clip.w
         height = clip.h
@@ -82,7 +124,7 @@ def editor(paths):
 #
 #
         clipwithborder = clip.margin(
-            top=int(difference/2), bottom=int(difference/2), color=(255, 255, 255))
+            top=int(difference/2), bottom=int(difference/2), color=(213, 213, 213))
 
         print(clipwithborder.size)
 #
@@ -91,54 +133,53 @@ def editor(paths):
 #
 #
         toptxtClip = TextClip(
-            title, color="black", font="Elephant", fontsize=22).margin(top=int(difference/4), opacity=0).set_pos(("center", "top"))
+            title, color="#4F4E4E", font="Inter-Bold", stroke_width=2, stroke_color="#4F4E4E", kerning=2, fontsize=45).margin(top=int(difference/4), opacity=0).set_pos(("center", "top")).set_duration(clip.duration)
 
-        toptxtClip = toptxtClip.set_pos("top").set_duration(clip.duration)
-#
+        # add wraptext, fontweight, fontcolor, fontsize, fontfamily to a new file
 #
 #
 #
 #
         bottomtxtClip = TextClip(
-            "CHEF FOOD", color="black", font="Elephant", fontsize=40).margin(left=int((width/5-20)), bottom=int(difference/4), opacity=0).set_pos(("center", "bottom")).set_duration(clip.duration)
+            ChannelName, color="#4F4E4E", font="Inter-Bold", stroke_width=2, stroke_color="#4F4E4E", kerning=2, fontsize=45).margin(bottom=int(difference/3), opacity=0).set_pos(("center", "bottom")).set_duration(clip.duration)
 #
 #
 #
 #
 #
-        logo = (ImageClip('F:\\Projects\\Python\\bots\\VideoEditor\\ReactionEditor\\assets\\logo.png')
+        logo = (ImageClip(r'F:\Projects\Python\bots\VideoEditor\Video Editor\assets\logo.png')
                 .set_duration(clip.duration)
                 .resize(height=80)
-                .margin(right=int(width/3), bottom=int(difference/4 - 10), opacity=0)
+                .margin(bottom=int(difference/7), opacity=0)
                 .set_pos("bottom"))
 #
 #
 #
 #
 #
-        rect = (ImageClip('F:\\Projects\\Python\\bots\\VideoEditor\\ReactionEditor\\assets\\rect.png').set_duration(
-            clip.duration).resize(height=90).margin(right=int(width/6*3+10), top=int(height/4*3), opacity=0).set_pos("center"))
+        # rect = (ImageClip('F:\\Projects\\Python\\bots\\VideoEditor\\ReactionEditor\\assets\\rect.png').set_duration(
+        # clip.duration).resize(height=90).margin(right=int(width/6*3+10), top=int(height/4*3), opacity=0).set_pos("center"))
 #
 #
 #
 #
 #
-        like = (ImageClip('F:\\Projects\\Python\\bots\\VideoEditor\\ReactionEditor\\assets\\like.png').set_duration(
-            clip.duration).resize(height=80).margin(right=int(width/6*5), top=int(height/4*3), opacity=0).set_pos("center"))
+        # like = (ImageClip(r'F:\Projects\Python\bots\VideoEditor\Video Editor\assets\logo.png').set_duration(
+        # clip.duration).resize(height=80).margin(right=int(width/6*5), top=int(height/4*3), opacity=0).set_pos("center"))
 #
 #
 #
 #
-#
-        likeText = TextClip(
-            "Please Like and Follow", color="black", font="Arial-Bold", fontsize=15).margin(right=int(width/6*3), top=int(height/5*3 + 80), opacity=0).set_pos("center").set_duration(clip.duration)
-#
+# #
+#         likeText = TextClip(
+#             "Please Like and Follow", color="black", font="Arial-Bold", fontsize=15).margin(right=int(width/6*3), top=int(height/5*3 + 80), opacity=0).set_pos("center").set_duration(clip.duration)
+# #
 #
 #
 #
 #
         audio = AudioFileClip(
-            'F:\\Projects\\Python\\bots\\VideoEditor\\ReactionEditor\\assets\\Drip.wav').set_duration(clip.duration).volumex(0.2).fx(afx.audio_fadein, 1)
+            r'F:\Projects\Python\bots\VideoEditor\Video Editor\assets\Drip.wav').set_duration(clip.duration).volumex(0.05).fx(afx.audio_fadein, 1)
 
         video_audio = clip.audio
 
@@ -147,11 +188,21 @@ def editor(paths):
         clipwithborder = clipwithborder.set_audio(final_audio)
 
         final = CompositeVideoClip(
-            [clipwithborder, toptxtClip, bottomtxtClip, logo, rect, like, likeText])
-        final.write_videofile("./videos/edited/" +
+            [clipwithborder, toptxtClip, bottomtxtClip, logo])
+        final.write_videofile("Video Editor/videos/edited/" +
                               title + ".mp4", fps=clip.fps)
 
         log.info("Video edited successfully")
+        try:
+            # move video to the edited folder
+            moveVideo(paths, FullEditedFolder)
+        except Exception as e:
+            print(e)
+            print("Error occured while moving the video")
+            log.error(e)
+
+        print("Video edited successfully \n\n\n")
+
     except Exception as e:
         print(e)
         log.error(e)
@@ -164,14 +215,77 @@ def editor(paths):
 #
 
 
-def main():
-    # get the path of the video file
-    # path = 'F:\\Projects\\Python\\bots\\VideoEditor\\ReactionEditor\\videos\\splited\\A wonderful recipe...Part 1.mp4'
-    path = 'F:\\Projects\\Python\\bots\\VideoEditor\\ReactionEditor\\videos\\short.mp4'
+def moveVideo(videoPath, FolderPath):
+    print("\nMoving video folder...")
 
-    editor(path)
-    # path = 'D:\\Videos For Uploading\\Village Tale\\Uploaded\\A wonderful recipe for fried chicken! must see.mp4'
-    # videoDivider(path)
+    try:
+        shutil.move(videoPath, FolderPath)
+        print("Moved to " + str(FolderPath) + "uploaded folder")
+    except Exception as e:
+        print("Error in moving video to uploaded folder" + str(e))
+        return
+
+
+def getSplittedVideos():
+    print("Getting videos")
+    path = r'F:\Projects\Python\bots\VideoEditor\Video Editor\videos\splited'
+    videos = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(".mp4"):
+                videos.append(os.path.join
+                              (root, file))
+    return videos
+
+
+# get video files from the folder
+def getUneditedVideos():
+    print("Getting videos")
+    try:
+        path = r'F:\Projects\Python\bots\VideoEditor\Video Editor\videos\Unedited'
+        videos = []
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file.endswith(".mp4"):
+                    videos.append(os.path.join(root, file))
+    except Exception as e:
+        print("Error getting videos")
+        print(e)
+        log.error(e)
+
+    return videos
+
+
+def menu():
+    print("1. Split videos")
+    print("2. Edit videos")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+
+    return choice
+
+
+def main():
+    # path = r'F:\Projects\Python\bots\VideoEditor\Video Editor\videos\splited\short.mp4'
+    # editor(path)
+    choice = menu()
+    try:
+        if choice == "1":
+            videos = getUneditedVideos()
+            print(videos)
+            for video in videos:
+                splitVideo(video)
+        elif choice == "2":
+            videos = getSplittedVideos()
+            print(videos)
+            for video in videos:
+                editor(video)
+        elif choice == "3":
+            sys.exit(1)
+    except Exception as e:
+        print(e)
+        print("Error occured")
+        sys.exit(1)
 
 
 if __name__ == '__main__':
